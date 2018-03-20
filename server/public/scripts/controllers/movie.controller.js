4e1fd951d15b927d6a1551b803af2f0b0577e5aa
Alex Aspell <alexgaspell@gmail.com>
2018-03-20 17:03:26 -0500

    flex-column added to movie page

--- server/public/scripts/controllers/movie.controller.js
+++ server/public/scripts/controllers/movie.controller.js
@@ -2,4 +2,5 @@ app.controller('MovieController', ['MovieService', function(MovieService){
     let self = this; 
 
     self.moviePage = MovieService.moviePage;
+    console.log('the movie', self.moviePage);
 }])
