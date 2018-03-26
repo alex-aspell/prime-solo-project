7e36996e2a2dd60d773df9537817f108c4ec13a0
Alex Aspell <alexgaspell@gmail.com>
2018-03-26 11:29:27 -0500

    display demographic chart and calculate demographic average

--- server/public/scripts/controllers/movie.controller.js
+++ server/public/scripts/controllers/movie.controller.js
@@ -11,24 +11,10 @@ app.controller('MovieController', ['MovieService', 'UserService', '$routeParams'
     self.enteredRating = MovieService.enteredRating; 
     self.enteredRating.user_id = UserService.userObject.id;
 
-    self.getDemographicInfo = function(demographics) {
-        //THIS IS NOT A POST. I HAD TO USE POST TO PASS UP ALL THE INFO IN DEMOGRAPHICS
-        self.demographics.movie_id = $routeParams.id
-        console.log(demographics);
-        $http({
-            method: 'POST', 
-            url: `/movies/demographics`,
-            data: demographics
-        }).then(function(response) {
-            console.log('demographics response', response.data)
-        }).catch(function(error) {
-            console.log('demographics error', error);
-        })
-    }
-    
     let id = $routeParams.id;
     //
     //Need to do a get using route params id that will get movie info from the API
+    
     //
     MovieService.getMovieAverage(id);
     MovieService.getRatingsForChart(id).then(function(ratingArray){
@@ -81,12 +67,84 @@ app.controller('MovieController', ['MovieService', 'UserService', '$routeParams'
                 fitModel: 'fit-width'
             }
         });
-        let el = document.getElementById( 'line' );
-        if( el ){
-            chart.renderTo(el);
-        } else{
-            let el2 = document.getElementById( 'thingy' );
-            console.log( 'couldn\'t find el', el2 );
+        chart.renderTo('#line');
+        
+    }
+
+    self.getDemographicInfo = function(demographics) {
+        //THIS IS NOT A POST. I HAD TO USE POST TO PASS UP ALL THE INFO IN DEMOGRAPHICS
+        self.demographics.movie_id = $routeParams.id
+        console.log(demographics);
+        $http({
+            method: 'POST', 
+            url: `/movies/demographics`,
+            data: demographics
+        }).then(function(response) {
+            console.log('demographics response', response.data);
+            let responseArray = response.data;
+            let ratingArray = [];
+            for (let i = 0; i<responseArray.length; i++) {
+                ratingArray.push(responseArray[i].rating);
+            }
+            self.createDemographicChartArrays(ratingArray);
+            self.createDemographicAverages(ratingArray);
+        }).catch(function(error) {
+            console.log('demographics error', error);
+        })
+    }
+
+    self.createDemographicAverages = function(ratingArray){
+        function getSum(total, num){
+            return total + num;
         }
+        let totalRating = ratingArray.reduce(getSum);
+        let demographicRating = Math.floor(totalRating/ratingArray.length);
+        moviePage.list.demographicRating = demographicRating; 
+    }
+
+    self.createDemographicChartArrays = function(ratingArray){ 
+        //function runs in .then of self.getRatingsForChart at line 70
+            let xAxis = []; 
+            let yAxis = []; 
+            let prev;
+            let dataset = [];
+            
+            
+            ratingArray.sort();
+            for ( let i = 0; i < ratingArray.length; i++ ) {
+                if ( ratingArray[i] !== prev ) {
+                    xAxis.push(ratingArray[i]);
+                    yAxis.push(1);
+                } else {
+                    yAxis[yAxis.length-1]++;
+                }
+                prev = ratingArray[i];
+                }
+            
+            for (let i = 0; i < xAxis.length; i++ ){
+                dataset.push({type: 'rating', x: xAxis[i], y: yAxis[i]})
+            }
+            console.log('dataset', dataset);
+            self.createDemographicChart(dataset);
+            
+    } 
+    self.createDemographicChart = function(dataset){
+        let chart = new tauCharts.Chart({
+            guide: {
+                y: {label: {text: '# of Ratings'}},
+                x: { min: 0, max: 100, tickFormat: 's', label:{text: 'Rating Scale'}},
+                showGridLines: 'y',
+                interpolate: 'smooth-keep-extremum'
+            },
+            data: dataset,
+            type: 'line',
+            x: 'x',
+            y: 'y',
+            color: 'red',
+            settings: {
+                fitModel: 'fit-width'
+            }
+        });
+        chart.renderTo('#line2');
     }
 }])
